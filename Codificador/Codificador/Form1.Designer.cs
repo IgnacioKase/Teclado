59a616cfbe9e4543898c2f4343d468b42a24dc77
IgnacioKase <33383172+IgnacioKase@users.noreply.github.com>
2017-11-09 23:28:20 -0300

    New: graphics midi New: serial midi coded output New: read midi from text Fixed: responsive designe

--- Codificador/Codificador/Form1.Designer.cs
+++ Codificador/Codificador/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
@@ -38,7 +39,13 @@
             this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
@@ -48,11 +55,11 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox1.Font = new System.Drawing.Font("Roboto Slab", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(12, 64);
+            this.textBox1.Location = new System.Drawing.Point(12, 74);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(607, 393);
+            this.textBox1.Size = new System.Drawing.Size(899, 199);
             this.textBox1.TabIndex = 0;
             // 
             // toolStrip1
@@ -62,7 +69,7 @@
             this.toolStripDropDownButton1});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(631, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(923, 25);
             this.toolStrip1.TabIndex = 2;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -82,7 +89,7 @@
             this.loadToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.textToolStripMenuItem});
             this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
-            this.loadToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
             this.loadToolStripMenuItem.Text = "Load";
             // 
             // textToolStripMenuItem
@@ -113,17 +120,56 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 279);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(923, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Resize += new System.EventHandler(this.pictureBox1_Resize);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 47);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.Click += new System.EventHandler(this.comboBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(631, 469);
+            this.ClientSize = new System.Drawing.Size(923, 479);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -140,6 +186,11 @@
         private System.Windows.Forms.ToolStripMenuItem textToolStripMenuItem;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Codificador/Codificador/Form1.cs
+++ Codificador/Codificador/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.Xml;
 using System.IO;
 using System.Collections;
+using System.IO.Ports;
 
 namespace Codificador
 {
@@ -22,6 +23,14 @@ namespace Codificador
         List<Int64> comandos = new List<Int64>();
         List<ThemesAppearance> temas = new List<ThemesAppearance>();
         short escala = 40;
+        Graphics papel;
+        Pen lapiz_negro_grueso = new Pen(Color.Black, 2);
+        Pen lapiz_rojo_grueso = new Pen(Color.Red, 2);
+        Pen lapiz_negro = new Pen(Color.Black, 1);
+        Color claro = Color.FromArgb(255, 240, 240, 240);
+        byte[] envio = new byte[] { };
+        string[] puertos = new string[] { };
+
         public Form1()
         {
             InitializeComponent();
@@ -35,15 +44,67 @@ namespace Codificador
                 }
                 toolStripComboBox1.Items.Add(_x.Name);
             }
-            rows = datos.Split(',');
+            Setear();
+            Recuadro();
+            Grilla();
+            /*rows = datos.Split(',');
             palabras = Palabra.ParseData(rows);
             comandos = Palabra.Codificar(palabras, escala);
             foreach (long _x in comandos)
             {
                 textBox1.AppendText(Convert.ToString(_x, 2));
                 textBox1.AppendText(Environment.NewLine);
+            }*/
+        }
+
+        private void CodificarMidi()
+        {
+            Limpiar();
+            Recuadro();
+            Grilla();
+            List<Palabra> palabras = new List<Palabra>() { Palabra.ReadFile() };
+            if (palabras.Last() != null)
+            {
+                while (palabras.Last().Tiempo == 0)
+                {
+                    palabras.Add(Palabra.ReadFile());
+                    if (palabras.Last() == null) break;
+                }
+                foreach (Palabra _x in palabras)
+                {
+                    if (_x != null)
+                    {
+                        foreach (int nota in _x.Nota)
+                        {
+                            papel.FillRectangle(new SolidBrush(Color.FromArgb(120, 255, 0, 0)), ((int)pictureBox1.Width / 127) * nota, 0, ((int)pictureBox1.Width / 127), pictureBox1.Height);
+                        }
+                    }
+                }
+                comandos = Palabra.Codificar(palabras, escala);
+                foreach (long _x in comandos)
+                {
+                    textBox1.AppendText(Convert.ToString(_x, 2));
+                    textBox1.AppendText(Environment.NewLine);
+                    if (serialPort1.IsOpen)
+                    {
+                        envio = SplitNumerInBytes(_x);
+                        serialPort1.Write(envio, 0, envio.Length);
+                        serialPort1.DiscardOutBuffer();
+                    }
+                }
+                
+                if (palabras.Last() != null)
+                {
+                    timer1.Interval = palabras.Last().Tiempo * 2;
+                    timer1.Enabled = true;
+                }else
+                {
+                    Limpiar();
+                    Recuadro();
+                    Grilla();
+                }  
             }
-            
+            Refresh();
         }
 
         private void toolStripComboBox1_TextChanged(object sender, EventArgs e)
@@ -57,18 +118,156 @@ namespace Codificador
             }
         }
 
+        public byte[] SplitNumerInBytes(long numero)
+        {
+            byte[] resultado = new byte[] { };
+            List<byte> bits = new List<byte>();
+            long contador = 0;
+            long backup = numero;
+            int i = 0;
+            while (backup != 0)
+            {
+                backup = backup >> 1;
+                contador++;
+            }
+            if (contador <= 8)
+            {
+                resultado = new byte[] { (byte)numero };
+            }
+            else
+            {
+                for (i = 0; (int)(contador - 8 * (i + 1)) > 0; i++)
+                {
+                    bits.Add((byte)(numero & 0xff));
+                    numero = numero >> 8;
+                }
+                if (numero != 0) bits.Add((byte)numero);
+                resultado = bits.ToArray();
+            }
+            return resultado;
+        }
+
         private void textToolStripMenuItem_Click(object sender, EventArgs e)
         {
             openFileDialog1.ShowDialog();
-            saveFileDialog1.ShowDialog();
-            saveFileDialog1.DefaultExt = ".txt";
-            Palabra.ParseDataByFile(openFileDialog1.FileName, saveFileDialog1.FileName);
+            //saveFileDialog1.ShowDialog();
+            //saveFileDialog1.DefaultExt = ".txt";
+            /*Palabra.ParseDataByFile(openFileDialog1.FileName, saveFileDialog1.FileName);
             Palabra.CodificarByFile(palabras, escala);
             textBox1.Text = "";
             foreach (long _x in comandos)
             {
                 textBox1.AppendText(Convert.ToString(_x, 2));
                 textBox1.AppendText(Environment.NewLine);
+            }*/
+            Palabra.OpenFile(openFileDialog1.FileName, saveFileDialog1.FileName);
+            CodificarMidi();
+        }
+
+        void Setear()
+        {
+
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            papel = Graphics.FromImage(pictureBox1.Image);
+            papel.Clear(claro);
+        }
+
+        void Limpiar()
+        {
+            papel.Clear(claro);
+        }
+
+        void Recuadro()
+        {
+            //Dibuja el recuadro
+            #region
+            papel.DrawLine(lapiz_negro_grueso, 1, 0, 1, pictureBox1.Height);
+            papel.DrawLine(lapiz_negro_grueso, 0, (pictureBox1.Height - 1), pictureBox1.Width,
+            (pictureBox1.Height - 1));
+            papel.DrawLine(lapiz_negro_grueso, 0, 1, pictureBox1.Width, 1);
+            papel.DrawLine(lapiz_negro_grueso, pictureBox1.Width - 1, 0, pictureBox1.Width -
+            1, pictureBox1.Height);
+            Refresh();
+            #endregion
+        }
+
+        void Dibujar()
+        {
+            try
+            {
+                Refresh();
+            }
+            catch
+            {
+
+            }
+        }
+
+        void Grilla()
+        {
+            //Dibuja la cuadricula
+            #region
+            for (int i = (pictureBox1.Width / 127); i < pictureBox1.Width; i += (pictureBox1.Width / 127))
+            {
+                papel.DrawLine(lapiz_negro, i, 0, i, pictureBox1.Height);
+            }
+            #endregion
+        }
+
+        private void pictureBox1_Resize(object sender, EventArgs e)
+        {
+            Setear();
+            Recuadro();
+            Grilla();
+            Refresh();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            CodificarMidi();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+                button1.Text = "Abrir";
+            }
+            else
+            {
+                serialPort1.Open();
+                button1.Text = "Cerrar";
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                try
+                {
+                    serialPort1.PortName = comboBox1.Text;
+                }
+                catch
+                {
+                    MessageBox.Show("Cierre el puerto para cambiar la dirección", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Cierre el puerto para cambiar la dirección", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void comboBox1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            puertos = SerialPort.GetPortNames();
+            for (int i = 0; i < puertos.Length; i++)
+            {
+                comboBox1.Items.Add(puertos[i]);
             }
         }
     }
@@ -80,13 +279,14 @@ namespace Codificador
         private List<int> velocidad;
         private int tiempo;
         static private int cantidad;
+        static StreamReader lectura;
+        static StreamWriter registros;
         #endregion
         #region Propiedades
         static public int Cantidad
         {
             get { return cantidad; }
         }
-
 
         public int Tiempo
         {
@@ -100,12 +300,14 @@ namespace Codificador
             set { velocidad = value; }
         }
 
-
         public List<int> Nota
         {
             get { return nota; }
             set { nota = value; }
         }
+
+        public static StreamReader Lectura { get => lectura; set => lectura = value; }
+        public static StreamWriter Registros { get => registros; set => registros = value; }
         #endregion
         #region Constructores
         public Palabra()
@@ -121,8 +323,9 @@ namespace Codificador
             cantidad++;
         }
         #endregion
+        //Viejos
         #region Métodos
-        static public List<Palabra> ParseData (string[] rows)
+        static public List<Palabra> ParseData(string[] rows)
         {
             List<Palabra> palabras = new List<Palabra>();
             bool isNew = true;
@@ -159,9 +362,9 @@ namespace Codificador
                 {
                     if (nota >= escala)
                     {
-                        #pragma warning disable CS0675 // Operador OR bit a bit utilizado en un operando de extensión de signo
+#pragma warning disable CS0675 // Operador OR bit a bit utilizado en un operando de extensión de signo
                         buffer = buffer | (1 << (byte)(nota - escala));
-                        #pragma warning restore CS0675 // Operador OR bit a bit utilizado en un operando de extensión de signo
+#pragma warning restore CS0675 // Operador OR bit a bit utilizado en un operando de extensión de signo
                     }
                 }
                 comandos.Add(buffer);
@@ -171,30 +374,30 @@ namespace Codificador
 
         static public void ParseDataByFile(string openFile, string saveFile)
         {
-            StreamReader lectura = new StreamReader(openFile);
-            StreamWriter registros = new StreamWriter(saveFile);
-            bool isNew = true;
-            Palabra palabras = new Palabra();
-            string palabra = "";
-            while ((palabra = lectura.ReadLine()) != null)
-            {
-                isNew = (palabras.Count == 0) || (palabras.LastOrDefault().Tiempo != (Int16.Parse(_x.Split()[0])));
-                if (isNew)
-                {
-                    Palabra line = new Palabra
-                    (
-                        Int16.Parse(_x.Split()[0]),
-                        new List<int>() { Int16.Parse(_x.Split()[3].Replace("n=", string.Empty)) },
-                        new List<int>() { Int16.Parse(_x.Split()[4].Replace("v=", string.Empty)) }
-                    );
-                    palabras.Add(line);
-                }
-                else
-                {
-                    palabras.Last().Nota.Add(Int16.Parse(_x.Split()[3].Replace("n=", string.Empty)));
-                    palabras.Last().Velocidad.Add(Int16.Parse(_x.Split()[4].Replace("v=", string.Empty)));
-                }
-            }
+            /* StreamReader lectura = new StreamReader(openFile);
+             StreamWriter registros = new StreamWriter(saveFile);
+             bool isNew = true;
+             Palabra palabras = new Palabra();
+             string palabra = "";
+             while ((palabra = lectura.ReadLine()) != null)
+             {
+                 isNew = (palabras.Count == 0) || (palabras.LastOrDefault().Tiempo != (Int16.Parse(_x.Split()[0])));
+                 if (isNew)
+                 {
+                     Palabra line = new Palabra
+                     (
+                         Int16.Parse(_x.Split()[0]),
+                         new List<int>() { Int16.Parse(_x.Split()[3].Replace("n=", string.Empty)) },
+                         new List<int>() { Int16.Parse(_x.Split()[4].Replace("v=", string.Empty)) }
+                     );
+                     palabras.Add(line);
+                 }
+                 else
+                 {
+                     palabras.Last().Nota.Add(Int16.Parse(_x.Split()[3].Replace("n=", string.Empty)));
+                     palabras.Last().Velocidad.Add(Int16.Parse(_x.Split()[4].Replace("v=", string.Empty)));
+                 }
+             }*/
         }
 
         static public List<Int64> CodificarByFile(List<Palabra> palabras, short escala)
@@ -216,6 +419,34 @@ namespace Codificador
                 comandos.Add(buffer);
             }
             return comandos;
+        }
+        #endregion
+        #region Métodos nuevos
+        static public bool OpenFile(string openFile, string saveFile)
+        {
+            Lectura = new StreamReader(openFile);
+            //Registros = new StreamWriter(saveFile);
+            return true;
+        }
+
+        static public Palabra ReadFile()
+        {
+            try
+            {
+                string dato = Lectura.ReadLine();
+                string[] rows = dato.Split(',');
+                Palabra palabra = new Palabra()
+                {
+                    tiempo = Int16.Parse(dato.Split()[0]),
+                    nota = new List<int>() { Int16.Parse(dato.Split()[3].Replace("n=", string.Empty)) },
+                    velocidad = new List<int>() { Int16.Parse(dato.Split()[4].Replace("v=", string.Empty)) }
+                };
+                return palabra;
+            }
+            catch
+            {
+                return null;
+            }
         }
         #endregion
     }
